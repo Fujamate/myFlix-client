5548fdad09ce271f172e11bb7b3c5061842b2983
Kevin Hölscher <kevin-hoelscher@gmx.de>
2023-09-19 09:38:46 +0200

    added a useEffect for fetching

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,33 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import PropTypes from 'prop-types'; 
+
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
+
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "The Expendables 3",
-      image:
-        "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ruW3malZtlg66ODfg614dFbXO68.jpg",
-      description: "Barney, Christmas and the rest of the team comes face-to-face with Conrad Stonebanks, who years ago co-founded The Expendables with Barney. Stonebanks subsequently became a ruthless arms trader and someone who Barney was forced to kill… or so he thought. "
-    },
-    {
-      id: 2,
-      title: "Oppenheimer",
-      image:
-        "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
-      description: "The story of J. Robert Oppenheimer’s role in the development of the atomic bomb during World War II."
-    },
-    {
-      id: 3,
-      title: "Oldboy",
-      image:
-        "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/pWDtjs568ZfOTMbURQBYuT4Qxka.jpg",
-      description: "With no clue how he came to be imprisoned, drugged and tortured for 15 years, a desperate businessman seeks revenge on his captors."
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch('https://myflixx-by-kevin-holscher.onrender.com/movies')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Movies from API: ", data);
+        const moviesFromApi = data.map((movie) => {
+          return {
+            _id: movie.id,
+            Title: movie.Title,
+            ImagePath: movie.ImagePath,
+            Description: movie.Description,
+            Genre: {
+              Name: movie.Genre.Name
+            },
+            Director: {
+              Name: movie.Director.Name
+            },
+            Featured: movie.Featured
+          };
+        });
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   if (selectedMovie) {
     return (
@@ -41,12 +46,14 @@ export const MainView = () => {
 
   
 
+  
+
 
   return (
     <div>
       {movies.map((movie) => (
           <MovieCard
-          key={movie.id}
+          key={movie.Title}
           movie={movie}
           onMovieClick={(newSelectedMovie) => {
             setSelectedMovie(newSelectedMovie);
